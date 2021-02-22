ebadbe42f9d2984500884a6f8864e27a85b175fe
Dharanish V <30527938+DharanishV@users.noreply.github.com>
2021-02-22 14:02:57 +0530

    Add files via upload

---
+++ narutoDownload.py
@@ -0,0 +1,26 @@
+import requests
+import os
+
+path = os.getcwd()
+
+os.mkdir(path+"/Naruto_Shippuden")
+
+
+episodeStartNumber = 389
+
+for episodeNumber in range(episodeStartNumber, 501):
+    print("inside loop")
+    url = f"http://nt.manga47.net/Naruto_Shippuuden_Dub/{episodeNumber}.mp4"
+    print(url)
+    response = requests.get(url, stream=True)
+    
+    fileName = "Naruto_Shipudden " + url.split('/')[-1]
+    
+    with open("Naruto_shippuden/"+fileName,'wb') as downloadFile:
+        
+        for i in response.iter_content(chunk_size = 1024*1024):
+            if i:
+                print("writing...")
+                downloadFile.write(i)
+            
+    print(fileName + " downloaded")
